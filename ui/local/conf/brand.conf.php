aa23a4585d5bd44a9a8c3a1f187ddf111762b3a1
Vismut <vismuttesla@gmail.com>
2025-10-22 10:30:35 +0500

    add needed files to git

---
+++ ui/local/conf/brand.conf.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'BRAND_LOGO' => '/assets/img/logo.svg',
+	'BRAND_LOGO_SIDEBAR' => '/assets/img/logo-sidebar.svg',
+	'BRAND_LOGO_SIDEBAR_COMPACT' => '/assets/img/logo-compact.svg',
+	'BRAND_FOOTER' => 'TAA Monitoring System - Powered by Zabbix',
+	'BRAND_HELP_URL' => 'https://www.zabbix.com/documentation/current/en/',
+	'BRAND_URL' => '#',
+	'BRAND_TITLE' => 'TAA Monitoring System'
+];
